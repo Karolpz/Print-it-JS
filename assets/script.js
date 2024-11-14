d6fe4f505878f9d998409ac5097808c816642bb2
Karolpz <carolinelpz34@gmail.com>
2024-11-14 14:10:30 +0100

    ajout com

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-let currentSlide = 0
-const slides = [
+//***TABLEAU DES IMAGES ***//
+let currentSlide = 0 //première image du tableau
+const slides = [//array des images et texte
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
@@ -16,55 +17,60 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+] 
 
-function updateCarousel() {
+
+//***CREATION DES BULLET POINTS ***//
+const DotsContainer = document.querySelector(".dots")//déclaration de la variable dotscontainer via la classe .dots(pour pouvoir manipuler/dynamiser)
+
+function CreateDots(){
+	slides.forEach((slide,index) => {// forEach = pour chaque élément de mon array, ajoute un point
+	const dot = document.createElement("div");
+	dot.classList.add("dot");//on associe la varibale dot à la classe .dot
+	DotsContainer.appendChild(dot);//on met cette classe .dot dans le parent dotsContainer
+	})
+}
+CreateDots();//appel de la fonction
+
+let DotSelected = document.querySelector(".dot")
+DotSelected.classList.add("dot_selected")//on met le dot le plus foncé à la première image
+
+
+//***CREATION DU CARROUSEL ***//
+function updateCarousel() {//fonction qui fait fonctionner le carrousel d'image
 
     const imageElement = document.querySelector(".banner-img");
-    imageElement.src = `./assets/images/slideshow/${slides[currentSlide].image}`;
+    imageElement.src = `./assets/images/slideshow/${slides[currentSlide].image}`;//chemin de l'image avec une variable d'ou le "`"
     
 
     const textElement = document.querySelector("#banner p");
-    textElement.innerHTML = slides[currentSlide].tagLine;
+    textElement.innerHTML = slides[currentSlide].tagLine; //le commentaire avec l'image
 
 
     const dots = document.querySelectorAll('.dot');
-    dots.forEach(dot => dot.classList.remove('dot_selected'));
-    dots[currentSlide].classList.add('dot_selected');
+    dots.forEach(dot => dot.classList.remove('dot_selected'));//on enlève le dot slectionné sur la première image
+    dots[currentSlide].classList.add('dot_selected');//on ajoute le dot selectionné selon l'image que l'on voit
 }
 
 const Clickflechedroite = document.querySelector(".arrow_right");
 Clickflechedroite.addEventListener("click", () => {
-	currentSlide = currentSlide + 1;
+	currentSlide = currentSlide + 1;//lorsqu'on click (eventlistener) flèche droite, alors on avance de +1 par rapporta l'array
 	if (currentSlide >= slides.length) {
-        currentSlide = 0;
+        currentSlide = 0;//SI la slide actuelle est au dela de la longueur du tableau, alors l'image revient a 0 (première image)
     }
-	updateCarousel ();
+	updateCarousel ();//execution de la fontion
 });
 
 const Clickflechegauche = document.querySelector(".arrow_left");
 Clickflechegauche.addEventListener("click", () => {
-	currentSlide = currentSlide - 1;
+	currentSlide = currentSlide - 1;//lorsqu'on click (eventlistener) flèche gauche, alors on recule de -1 par rapporta l'array
 	if (currentSlide < 0) {
-        currentSlide = slides.length - 1;
+        currentSlide = slides.length - 1;//SI la slide actuelle est inférieur à 0 (avant la première image), alors l'image revient a la dernière image
     }
-	updateCarousel ();
+	updateCarousel ();//execution de la fontion
 });
 
-const DotsContainer = document.querySelector(".dots")
 
-function CreateDots(){
-	slides.forEach((slide,index) => {
-	const dot = document.createElement("div");
-	dot.classList.add("dot");
-	DotsContainer.appendChild(dot);
-	})
-}
-
-CreateDots();
-
-let DotSelected = document.querySelector(".dot")
-DotSelected.classList.add("dot_selected")
 
 
 
